433872575e98a4407651648bdbc0eeb778b24a99
Najib Fikri <najibfikri13@gmail.com>
2022-03-31 21:36:34 +0700

    Create auth_page_register.dart

---
+++ lib/auth_page_regsiter.dart
@@ -0,0 +1,238 @@
+import "package:flutter/material.dart";
+import 'package:flutter/services.dart';
+
+
+class AuthPageRegister extends StatelessWidget {
+  const AuthPageRegister({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: const EdgeInsets.only(
+        top: 35,
+        left: 50,
+        right: 50,
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextField(
+              keyboardType: TextInputType.name,
+              enableSuggestions: false,
+              autocorrect: false,
+
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 0),
+                prefixIcon: const Icon(
+                  Icons.person_outline,
+                  color: Color(0xff626663),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xff626663),
+                  ),
+                ),
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xff626663),
+                  ),
+                ),
+                hintText: "Nama Lengkap",
+                hintStyle: const TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              enableSuggestions: false,
+              autocorrect: false,
+
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 0),
+                prefixIcon: const Icon(
+                  Icons.email_outlined,
+                  color: Color(0xff626663),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xff626663),
+                  ),
+                ),
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xff626663),
+                  ),
+                ),
+                hintText: "Email",
+                hintStyle: const TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              keyboardType: TextInputType.phone,
+              enableSuggestions: false,
+              autocorrect: false,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+              ],
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 0),
+                prefixIcon: const Icon(
+                  Icons.phone_outlined,
+                  color: Color(0xff626663),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xff626663),
+                  ),
+                ),
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xff626663),
+                  ),
+                ),
+                hintText: "No. Handphone",
+                hintStyle: const TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 0),
+                prefixIcon: const Icon(
+                  Icons.lock_outlined,
+                  color: Color(0xff626663),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xff626663),
+                  ),
+                ),
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xff626663),
+                  ),
+                ),
+                hintText: "Masukkan Password",
+                hintStyle: const TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 0),
+                prefixIcon: const Icon(
+                  Icons.lock_outlined,
+                  color: Color(0xff626663),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xff626663),
+                  ),
+                ),
+                enabledBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xff626663),
+                  ),
+                ),
+                hintText: "Masukkan Ulang Password",
+                hintStyle: const TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            InkWell(
+              onTap: () {
+                print("yuhu 'masuk' diclick");
+              },
+              child: Ink(
+                width: double.infinity,
+                height: 45,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color(0xff2f4858),
+                ),
+                child: const Center(
+                  child: Text(
+                    "Daftar",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            RichText(
+              text: const TextSpan(
+                text: "Sudah punya akun? ",
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                ),
+                children: <TextSpan> [
+                  TextSpan(
+                    text: "Masuk",
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold
+                    )
+                  )
+                ],
+              )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
